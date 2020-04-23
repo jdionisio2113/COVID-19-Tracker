7febf8e01be4c398c6f155f43311755068804e92
Justin Dionisio <justindionisio21@yahoo.com>
2020-04-22 19:28:08 -0700

    Fetched COVID data and created components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import api from '../config/api';
+import axios from 'axios';
+import { all, get } from "axios";
+import Cards from "./Cards";
+import Chart from "./Chart";
+import CountryPicker from "./CountryPicker";
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.fetchCovidData = this.fetchCovidData.bind(this);
+    }
+
+    componentDidMount() {
+
+        this.fetchCovidData();
+
+    }
+
+
+    fetchCovidData() {
+
+        all([api.get(), api.get("/countries")]).then(
+            res => {
+                var globalCount = res[0].data;
+                var countries = res[1].data;
+
+                console.log(globalCount);
+                console.log(countries)
+            }
+
+        )
+    }
+
     render() {
         return (
-            <div>Hello</div>
+            <div>
+                <h1>COVID-19</h1>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
         )
     }
 }
